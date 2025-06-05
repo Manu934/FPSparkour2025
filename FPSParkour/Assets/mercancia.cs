b297860e35be48a10e8b4717b8b9bf0dce1f5b03
Tomi <141858115+tomiBraunn@users.noreply.github.com>
2025-06-05 12:21:31 -0300

    funcuion plata funca

--- FPSParkour/Assets/mercancia.cs
+++ FPSParkour/Assets/mercancia.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class mercancia : MonoBehaviour
 {
+    public float cost;
+    public MoneyManager moneyManager;
+
+    void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Player")
         {
-        Destroy(gameObject);
+            //alcualizar dinero por la compra
+            moneyManager.UpdateMoney(-cost);
+            Destroy(gameObject);
         }
     }
 }
